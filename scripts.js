ac7505578cd9731b244ab9fce50547052819c586
LudwigStumpp <ludwig.stumpp@web.de>
2020-05-22 18:52:07 +0200

    neue Einträge hinzugefügt und image-copyright

--- scripts.js
+++ scripts.js
@@ -17,13 +17,14 @@ $(document).ready(function () {
     $.each(data, function (key, val) {
       const imgUrl = val.imgUrl;
       const backgroundPosition = val.backgroundPosition;
+      const imgCopyright = val.imgCopyright;
       const name = val.name;
       const text = val.text;
       const link = val.link;
 
       style = 'background-image:url(${imgUrl})'
 
-      const project = `<div class="project" style='background-image:url(${imgUrl}); background-position:${backgroundPosition}' name='${name}' text='${text}' link='${link}'>
+      const project = `<div class="project" style='background-image:url(${imgUrl}); background-position:${backgroundPosition}' imgCopyright='${imgCopyright}' name='${name}' text='${text}' link='${link}'>
           <div class="info">
             <div class="title">${name}</div>
             <div class="button modal-link">More</div>
@@ -39,11 +40,12 @@ $(document).ready(function () {
   projects.on('click', '.modal-link', function () {
     const project = $(this)[0].classList[0] == 'project' ? $(this) : $(this).parents('.project')
     const name = project.attr('name');
+    const imgCopyright = project.attr('imgCopyright');
     const text = project.attr('text');
     const link = project.attr('link');
 
     modal.find('.title').text(name);
-    modal.find('.text').text(text);
+    modal.find('.text').html(`${text}<br><span class="img-copyright">(${imgCopyright})</span>`);
     modal.find('.link').attr('href', link);
     modal.css("display", "block");
   })
